af3cf54a847014c2e56e654ba8d1df5b140f83ce
Ziji <104454302+zijipia@users.noreply.github.com>
2026-09-03 23:48:36 +0900

    types: add manager seek and trackStuck events

--- core/src/types/manager-events.ts
+++ core/src/types/manager-events.ts
@@ -4,6 +4,7 @@ import type { Track } from "./core";
 declare module "./core" {
 	interface ManagerEvents {
 		seek: [player: Player, payload: { track: Track; position: number }];
+		trackStuck: [player: Player, track: Track | null];
 	}
 }
 
